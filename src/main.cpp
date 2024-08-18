e3b126d771a17e1118414fc6550ed0df16501b9d
Zimin <18171190533@163.com>
2024-08-18 14:40:05 +0000

    Init
    Signed-off-by: Zimin <18171190533@163.com>

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <atomic>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <thread>
+#include <vector>
+#include <alsa/asoundlib.h>
+
+int main(int argc, char **argv) {
+
+  printf("test!\r\n");
+
+  return 0;
+}
